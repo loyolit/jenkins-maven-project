b6ce1c4c6058143fa82fb346b7d0c1c51b4829c8
loyolit <loyolite171907@gmail.com>
2024-03-20 08:20:03 +0000

    initial commit for webapp project

---
+++ src/main/java/com/sample/web/App.java
@@ -0,0 +1,13 @@
+package com.sample.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
